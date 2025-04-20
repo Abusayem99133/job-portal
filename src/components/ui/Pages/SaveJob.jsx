88bbf68ceae35210d690d263613f924e6b93c112
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-04-21 05:55:33 +0600

    modify to the save job page

--- src/components/ui/Pages/SaveJob.jsx
+++ src/components/ui/Pages/SaveJob.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const SaveJob = () => {
   return (
     <div>
-      <h3>This is save job</h3>
+      <h3>This is My save job</h3>
+      <p>it was a most important page</p>
     </div>
   );
 };
