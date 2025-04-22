4abeeafcbdb8e2d45b54d1400dc8903a750bf304
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-04-22 21:35:19 +0600

    updated

--- src/components/ui/Pages/JobPost.jsx
+++ src/components/ui/Pages/JobPost.jsx
@@ -6,6 +6,7 @@ const JobPost = () => {
       <h3>This is job post</h3>
       <h3>it was my last work in this</h3>
       <h3>it was my last work in this</h3>
+      <h3>it was my last work in this</h3>
     </div>
   );
 };
