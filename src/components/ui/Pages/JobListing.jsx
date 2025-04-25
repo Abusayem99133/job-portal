a00fe6a36112ccb052423e8ba94406cbaf9fc609
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-04-25 22:59:55 +0600

    update to the joblisting page in data

--- src/components/ui/Pages/JobListing.jsx
+++ src/components/ui/Pages/JobListing.jsx
@@ -1,22 +1,47 @@
 import { getJobs } from "@/api/apiJobs";
-import { useSession } from "@clerk/clerk-react";
-import React, { useEffect } from "react";
+import useFetch from "@/hook/useFatch";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { BarLoader } from "react-spinners";
+import JobCard from "./JobCard";
 
 const JobListing = () => {
-  const { session } = useSession();
-  const fetchJobs = async () => {
-    const supabaseAccessToken = await session.getToken({
-      template: "supabase",
-    });
-    const data = await getJobs(supabaseAccessToken);
-    console.log(data);
-  };
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+  const { isLoaded } = useUser();
+  const {
+    fn: fnJobs,
+    data: jobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
   useEffect(() => {
-    fetchJobs();
-  }, []);
+    if (isLoaded) fnJobs();
+  }, [isLoaded, location, company_id, searchQuery]);
   return (
     <div>
-      <h3>This is job Listing page</h3>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+      {/* add filters here */}
+      {/* {loadingJobs && (
+        <BarLoader className="mt-4 " width={"100%"} color="#36d7b7" />
+      )} */}
+      {loadingJobs === false && (
+        <div>
+          {jobs?.length ? (
+            jobs?.map((job) => {
+              return <JobCard key={job?.id} job={job} />;
+            })
+          ) : (
+            <div>No Jobs Found 😥</div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
