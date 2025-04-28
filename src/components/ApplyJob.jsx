ea8d5fd4bae9ab4e811cb6e130f7b948bff86886
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-04-28 16:36:14 +0600

    update to the apply button form

--- src/components/ApplyJob.jsx
+++ src/components/ApplyJob.jsx
@@ -10,10 +10,13 @@ import {
   DrawerTrigger,
 } from "./ui/drawer";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
 
 const ApplyJob = ({ user, job, applied = false, fetchJob }) => {
   return (
-    <div className="w-full sm:w-auto">
+    <div>
       <Drawer open={applied ? false : undefined}>
         <DrawerTrigger asChild>
           <Button
@@ -34,6 +37,33 @@ const ApplyJob = ({ user, job, applied = false, fetchJob }) => {
               Please fill out the form below carefully.
             </DrawerDescription>
           </DrawerHeader>
+
+          <form className="flex flex-col gap-4 p-4 pb-0">
+            <Input
+              type="number"
+              placeholder="Years of Experience"
+              className="flex-1"
+            />
+            <Input
+              type="text"
+              placeholder="Skills (Comma Separated)"
+              className="flex-1"
+            />
+            <RadioGroup defaultValue="option-one">
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="Intermediate" id="intermediate" />
+                <Label htmlFor="intermediate">Intermediate</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="Graduate" id="graduate" />
+                <Label htmlFor="graduate">Graduate</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem value="Post Graduate" id="post-graduate" />
+                <Label htmlFor="post-graduate">Post Graduate</Label>
+              </div>
+            </RadioGroup>
+          </form>
           <DrawerFooter className="flex flex-col gap-4">
             <Button className="w-full">Submit</Button>
             <DrawerClose>
