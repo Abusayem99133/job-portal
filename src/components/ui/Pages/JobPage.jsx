426b94c0c7a2b303a6bd3ece29e8ad1a62ea88c2
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-04-28 11:21:45 +0600

    install the mdeditor and setup to the jobpage  and show to the ui

--- src/components/ui/Pages/JobPage.jsx
+++ src/components/ui/Pages/JobPage.jsx
@@ -1,6 +1,8 @@
 import { getSingleJob } from "@/api/apiJobs";
 import useFetch from "@/hook/useFatch";
 import { useUser } from "@clerk/clerk-react";
+import MDEditor from "@uiw/react-md-editor";
+import { Briefcase, DoorClosed, DoorOpen, MapPinIcon } from "lucide-react";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { BarLoader } from "react-spinners";
@@ -23,11 +25,41 @@ const JobPage = () => {
     return <BarLoader className="mb-4" width={"100%"} color="" />;
   }
   return (
-    <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
-      <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
-        {job?.title}
-      </h1>
-      <img src={job?.company?.logo_url} className="h-12" alt={job?.title} />
+    <div className="flex flex-col gap-8 mt-5">
+      <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
+        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
+          {job?.title}
+        </h1>
+        <img src={job?.company?.logo_url} className="h-12" alt={job?.title} />
+      </div>
+      <div className="flex justify-between ">
+        <div>
+          <MapPinIcon />
+          {job?.location}
+        </div>
+        <div className="flex gap-2">
+          <Briefcase />
+          {job?.application?.length} Applicants
+        </div>
+        <div className="flex gap-2">
+          {job?.isOpen ? (
+            <>
+              <DoorOpen /> Open
+            </>
+          ) : (
+            <>
+              <DoorClosed /> Closed
+            </>
+          )}
+        </div>
+      </div>
+      {/* hiring status  */}
+      <h2 className="text-2xl sm:text-3xl font-bold">About the job</h2>
+      <p className="sm:text-lg ">{job?.description}</p>
+      <h2 className="text-2xl sm:text-3xl font-bold">
+        What we are looking for
+      </h2>
+      <MDEditor.Markdown source={job?.requirments} />
     </div>
   );
 };
