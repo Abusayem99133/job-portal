d7df720ef16a630bdc4acd560d395808f822a3f2
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-04-28 22:34:22 +0600

    update to the apply button in resume form

--- src/components/ApplyJob.jsx
+++ src/components/ApplyJob.jsx
@@ -13,8 +13,61 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
 import { Label } from "./ui/label";
-
+import { Controller, useForm } from "react-hook-form";
+import useFetch from "@/hook/useFatch";
+import { applyToJob } from "@/api/apiApplication";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+const schema = z.object({
+  experience: z
+    .number()
+    .min(0, { message: "Experience must be at least 0" })
+    .int(),
+  skills: z.string().min(1, { message: "Skills are required" }),
+  education: z.enum(["Intermediate", "Graduate", "Post Graduate"], {
+    message: "Education is required",
+  }),
+  resume: z
+    .any()
+    .refine(
+      (file) =>
+        file[0] &&
+        (file[0].type === "application/pdf" ||
+          file[0].type === "application/msword"),
+      {
+        message: "Only PDF of Word documents are allowed",
+      }
+    ),
+});
 const ApplyJob = ({ user, job, applied = false, fetchJob }) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
+  const {
+    loading: loadingApply,
+    error: errorApply,
+    fn: fnApply,
+  } = useFetch(applyToJob);
+
+  const onSubmit = (data) => {
+    fnApply({
+      ...data,
+      job_id: job.id,
+      candidate_id: user.id,
+      name: user.fullname,
+      status: "applied",
+      resume: data.resume[0],
+    }).then(() => {
+      fetchJob();
+      reset();
+    });
+  };
   return (
     <div>
       <Drawer open={applied ? false : undefined}>
@@ -38,35 +91,72 @@ const ApplyJob = ({ user, job, applied = false, fetchJob }) => {
             </DrawerDescription>
           </DrawerHeader>
 
-          <form className="flex flex-col gap-4 p-4 pb-0">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 p-4 pb-0"
+          >
             <Input
               type="number"
               placeholder="Years of Experience"
               className="flex-1"
+              {...register("experience", { valueAsNumber: true })}
             />
+
+            {errors.experience && (
+              <p className="text-red-500">{errors.experience.message}</p>
+            )}
             <Input
               type="text"
               placeholder="Skills (Comma Separated)"
               className="flex-1"
+              {...register("skills")}
             />
-            <RadioGroup defaultValue="option-one">
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="Intermediate" id="intermediate" />
-                <Label htmlFor="intermediate">Intermediate</Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="Graduate" id="graduate" />
-                <Label htmlFor="graduate">Graduate</Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="Post Graduate" id="post-graduate" />
-                <Label htmlFor="post-graduate">Post Graduate</Label>
-              </div>
-            </RadioGroup>
+            {errors.skills && (
+              <p className="text-red-500">{errors.skills.message}</p>
+            )}
+
+            <Controller
+              name="education"
+              control={control}
+              render={({ field }) => (
+                <RadioGroup onValueChange={field.onChange} {...field}>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="Intermediate" id="intermediate" />
+                    <Label htmlFor="intermediate">Intermediate</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="Graduate" id="graduate" />
+                    <Label htmlFor="graduate">Graduate</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="Post Graduate" id="post-graduate" />
+                    <Label htmlFor="post-graduate">Post Graduate</Label>
+                  </div>
+                </RadioGroup>
+              )}
+            />
+
+            {errors.experience && (
+              <p className="text-red-500">{errors.experience.message}</p>
+            )}
+            <Input
+              type="file"
+              accept=".pdf, .doc, .docx"
+              className="flex-1 file:text-gray-500"
+              {...register("resume")}
+            />
+            {errors.resume && (
+              <p className="text-red-500">{errors.resume.message}</p>
+            )}
+            {errorApply?.message && (
+              <p className="text-red-500">{errorApply.message}</p>
+            )}
+            <Button type="submit" variant="blue" size="lg" className="w-full">
+              Submit
+            </Button>
           </form>
           <DrawerFooter className="flex flex-col gap-4">
-            <Button className="w-full">Submit</Button>
-            <DrawerClose>
+            <DrawerClose asChild>
               <Button className="w-full" variant="outline">
                 Cancel
               </Button>
