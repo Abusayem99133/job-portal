abb0972cafbb1d340122277988425f43abb91ff5
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2025-04-30 14:35:24 +0600

    update to the post of job in recruiter

--- src/components/ui/Pages/JobPost.jsx
+++ src/components/ui/Pages/JobPost.jsx
@@ -3,7 +3,7 @@ import useFetch from "@/hook/useFatch";
 import { useUser } from "@clerk/clerk-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Input } from "../input";
 
@@ -17,8 +17,11 @@ import {
   SelectValue,
 } from "../select";
 import { State } from "country-state-city";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { Textarea } from "../textarea";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "../button";
+import { addNewJob } from "@/api/apiJobs";
 const schema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   description: z.string().min(1, { message: "Description is required" }),
@@ -28,6 +31,7 @@ const schema = z.object({
 });
 
 const JobPost = () => {
+  const navigate = useNavigate();
   const { user, isLoaded } = useUser();
   const {
     register,
@@ -50,57 +54,130 @@ const JobPost = () => {
   useEffect(() => {
     if (isLoaded) fnCompanies();
   }, [isLoaded]);
+
+  const {
+    loading: loadingCreateJob,
+    error: errorCreateJob,
+    data: dataCreateJbo,
+    fn: fnCreateJob,
+  } = useFetch(addNewJob);
+
+  const onSubmit = (data) => {
+    fnCreateJob({
+      ...data,
+      recruiter_id: user.id,
+      isOpen: true,
+    });
+  };
+
+  useEffect(() => {
+    if (dataCreateJbo?.length > 0) navigate("/jobs-listing");
+  }, [loadingCreateJob]);
   if (!isLoaded || loadingCompanies) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
   if (user?.unsafeMetadata?.role !== "recruiter") {
-    return <Navigate to={"/jobs"} />;
+    return <Navigate to={"/jobs-listing"} />;
   }
   return (
     <div>
       <h1 className="gradient-title font-extrabold text-5xl sm:text-7xl text-center pb-8">
         Post a Job
       </h1>
-      <form>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 p-4 pb-0"
+      >
         <Input placeholder="Job Title" {...register("title")} />
         {errors.title && <p className="text-red-500">{errors.title.message}</p>}
         <Textarea placeholder="Job Description" {...register("description")} />
         {errors.description && (
           <p className="text-red-500">{errors.description.message}</p>
         )}
+
+        <div className="flex gap-4 items-center">
+          <Controller
+            name="location"
+            control={control}
+            render={({ field }) => (
+              <Select value={field.value} onValueChange={field.onChange}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Filter by Location" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {State?.getStatesOfCountry("BD").map(({ name }) => {
+                      return (
+                        <SelectItem key={name} value={name}>
+                          {name}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+
+          <Controller
+            name="company_id"
+            control={control}
+            render={({ field }) => (
+              <Select value={field.value} onValueChange={field.onChange}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Filter by Company">
+                    {field.value
+                      ? companies?.find((com) => com.id === Number(field.value))
+                          ?.name
+                      : "Company"}
+                  </SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {companies?.map(({ name, id }) => {
+                      return (
+                        <SelectItem key={name} value={id}>
+                          {name}
+                        </SelectItem>
+                      );
+                    })}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            )}
+          />
+
+          {/* add company drawer */}
+
+          {errors.location && (
+            <p className="text-red-500">{errors.location.message}</p>
+          )}
+
+          {errors.company_id && (
+            <p className="text-red-500">{errors.company_id.message}</p>
+          )}
+        </div>
+        <Controller
+          name="requirement"
+          control={control}
+          render={({ field }) => (
+            <MDEditor value={field.value} onChange={field.onChange} />
+          )}
+        />
+        {errors.requirement && (
+          <p className="text-red-500">{errors.requirement.message}</p>
+        )}
+        {errors.errorCreateJob && (
+          <p className="text-red-500">{errors?.errorCreateJob?.message}</p>
+        )}
+        {errorCreateJob?.message && (
+          <p className="text-red-500">{errorCreateJob?.message}</p>
+        )}
+        {loadingCreateJob && <BarLoader width={"100%"} color="#36d7b7" />}
+        <Button type="submit" variant="blue" size="lg" className="mt-2">
+          Submit
+        </Button>
       </form>
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder="Filter by Location" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {State?.getStatesOfCountry("BD").map(({ name }) => {
-              return (
-                <SelectItem key={name} value={name}>
-                  {name}
-                </SelectItem>
-              );
-            })}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder="Filter by Company" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            {companies?.map(({ name, id }) => {
-              return (
-                <SelectItem key={name} value={name}>
-                  {name}
-                </SelectItem>
-              );
-            })}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
     </div>
   );
 };
